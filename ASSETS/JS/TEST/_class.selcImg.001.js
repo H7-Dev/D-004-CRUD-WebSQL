8d8334036e72da5e693ddc267b72140cf17d37ab
Herbert <herbert.dev.alfa@gmail.com>
2023-02-12 14:12:16 -0300

    01.28 [-g update] _class.selcImg.001

--- ASSETS/JS/TEST/_class.selcImg.001.js
+++ ASSETS/JS/TEST/_class.selcImg.001.js
@@ -1,43 +1,51 @@
 class ImageSelector {
     constructor(inputFileSelector, previewSelector, unit = 'bytes') {
-      this.inputFile = document.querySelector(inputFileSelector);
-      this.preview = document.querySelector(previewSelector);
-      this.unit = unit;
-      this.inputFile.addEventListener('change', this.handleInputFileChange.bind(this));
+        this.inputFile = document.querySelector(inputFileSelector);
+        this.preview = document.querySelector(previewSelector);
+        this.unit = unit;
+        this.inputFile.addEventListener('change', this.handleInputFileChange.bind(this));
     }
 
     handleInputFileChange() {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.preview.src = e.target.result;
-        this.file = this.inputFile.files[0];
-        this.fileName = this.file.name;
-        this.fileSize = this.file.size;
-        this.fileSizeInSelectedUnit = this.convertToSelectedUnit(this.file.size);
-        this.fileType = this.file.type;
-        document.querySelector('#file-info').innerHTML = `
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            this.preview.src = e.target.result;
+            this.file = this.inputFile.files[0];
+            this.fileName = this.file.name;
+            this.fileSize = this.file.size;
+            this.fileSizeInSelectedUnit = this.convertToSelectedUnit(this.file.size);
+            this.fileType = this.file.type;
+            document.querySelector('#file-info').innerHTML = `
           Nome: ${this.fileName} <br>
-          Tamanho: ${this.fileSizeInSelectedUnit} ${this.unit} <br>
+          Tamanho: ${this.humanizeSize(this.fileSizeInSelectedUnit)} ${this.unit} <br>
           Tipo: ${this.fileType}
         `;
-      };
-      reader.readAsDataURL(this.inputFile.files[0]);
+        };
+        reader.readAsDataURL(this.inputFile.files[0]);
     }
 
     convertToSelectedUnit(sizeInBytes) {
-      switch (this.unit) {
-        case 'bytes':
-          return sizeInBytes;
-        case 'kilobytes':
-          return sizeInBytes / 1024;
-        case 'megabytes':
-          return sizeInBytes / (1024 * 1024);
-        case 'gigabytes':
-          return sizeInBytes / (1024 * 1024 * 1024);
-        default:
-          return sizeInBytes;
-      }
+        switch (this.unit) {
+            case 'bytes':
+                return sizeInBytes;
+            case 'kilobytes':
+                return sizeInBytes / 1024;
+            case 'megabytes':
+                return sizeInBytes / (1024 * 1024);
+            case 'gigabytes':
+                return sizeInBytes / (1024 * 1024 * 1024);
+            default:
+                return sizeInBytes;
+        }
     }
-  }
 
-  const imageSelector = new ImageSelector('.input-file', '#preview', 'megabytes');
+    humanizeSize(size) {
+        return new Intl.NumberFormat().format(size);
+    }
+}
+
+const imageSelector = new ImageSelector(
+    '.input-file',
+    '#preview',
+    'megabytes'
+)
