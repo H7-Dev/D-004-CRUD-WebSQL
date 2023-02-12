4a2592389d5ff10b336a2be1daeb9aa257d21ea1
Herbert <herbert.dev.alfa@gmail.com>
2023-02-12 11:31:40 -0300

    01.25 [-g update] _class.selcImg.001

--- ASSETS/JS/TEST/_class.selcImg.001.js
+++ ASSETS/JS/TEST/_class.selcImg.001.js
@@ -1,10 +1,18 @@
-const inputFile = document.querySelector('.input-file');
-const preview = document.querySelector('#preview');
+console.log('ImageSelector')
+class ImageSelector {
+    constructor(inputFileSelector, previewSelector) {
+        this.inputFile = document.querySelector(inputFileSelector);
+        this.preview = document.querySelector(previewSelector);
+        this.inputFile.addEventListener('change', this.handleInputFileChange.bind(this));
+    }
 
-inputFile.addEventListener('change', function() {
-  const reader = new FileReader();
-  reader.onload = function(e) {
-    preview.src = e.target.result;
-  }
-  reader.readAsDataURL(inputFile.files[0]);
-});
+    handleInputFileChange() {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            this.preview.src = e.target.result;
+        };
+        reader.readAsDataURL(this.inputFile.files[0]);
+    }
+}
+
+const imageSelector = new ImageSelector('.input-file', '#preview');
