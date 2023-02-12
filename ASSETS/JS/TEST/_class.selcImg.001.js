7986512f9a2628c6e7b530a14093460adc19792f
Herbert <herbert.dev.alfa@gmail.com>
2023-02-12 11:36:49 -0300

    01.26 [-g update] _class.selcImg.001

--- ASSETS/JS/TEST/_class.selcImg.001.js
+++ ASSETS/JS/TEST/_class.selcImg.001.js
@@ -1,4 +1,3 @@
-console.log('ImageSelector')
 class ImageSelector {
     constructor(inputFileSelector, previewSelector) {
         this.inputFile = document.querySelector(inputFileSelector);
@@ -10,6 +9,15 @@ class ImageSelector {
         const reader = new FileReader();
         reader.onload = (e) => {
             this.preview.src = e.target.result;
+            this.file = this.inputFile.files[0];
+            this.fileName = this.file.name;
+            this.fileSize = this.file.size;
+            this.fileType = this.file.type;
+            document.querySelector('#file-info').innerHTML = `
+          Nome: ${this.fileName} <br>
+          Tamanho: ${this.fileSize} bytes <br>
+          Tipo: ${this.fileType}
+        `;
         };
         reader.readAsDataURL(this.inputFile.files[0]);
     }
