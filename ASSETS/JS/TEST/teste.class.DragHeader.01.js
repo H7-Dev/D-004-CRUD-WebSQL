168cc90196eed6590e77bf9d196ccfc03bf4fded
Herbert <herbert.dev.alfa@gmail.com>
2023-02-14 12:30:26 -0300

    ❗01.36 [-g , save] teste.class.DragHeader1.1

--- ASSETS/JS/TEST/teste.class.DragHeader.01.js
+++ ASSETS/JS/TEST/teste.class.DragHeader.01.js
@@ -1,41 +1,26 @@
-class DragAndDrop {
-    constructor(wrapperSelector, headerSelector) {
-        this.wrapper = document.querySelector(wrapperSelector);
-        this.header = this.wrapper.querySelector(headerSelector);
-        this.originalLeft = this.wrapper.offsetLeft;
-        this.originalTop = this.wrapper.offsetTop;
-        this.isDragging = false;
-    }
-    onDrag({
-        movementX,
-        movementY
-    }) {
-        if (!this.isDragging) return;
+const wrapper = document.querySelector("body > div.form"),
+     header = wrapper.querySelector("body > div.form > div > div.header");
 
-        let leftVal = parseInt(this.wrapper.style.left || this.originalLeft);
-        let topVal = parseInt(this.wrapper.style.top || this.originalTop);
-        this.wrapper.style.left = `${leftVal + movementX}px`;
-        this.wrapper.style.top = `${topVal + movementY}px`;
-    }
-    resetPosition() {
-        this.wrapper.style.left = `${this.originalLeft}px`;
-        this.wrapper.style.top = `${this.originalTop}px`;
-    }
-    handleHeaderMouseDown() {
-        this.header.classList.add("activeMove");
-        this.isDragging = true;
-        this.header.addEventListener("mousemove", this.onDrag.bind(this));
-    }
-    handleDocumentMouseUp() {
-        this.header.classList.remove("activeMove");
-        this.header.removeEventListener("mousemove", this.onDrag);
-        this.isDragging = false;
-    }
-    enable() {
-        this.header.addEventListener("mousedown", this.handleHeaderMouseDown.bind(this));
-        document.addEventListener("mouseup", this.handleDocumentMouseUp.bind(this));
-        this.header.addEventListener("dblclick", this.resetPosition.bind(this));
-    }
+function onDrag({
+    movementX,
+    movementY
+}) {
+    console.log('=> 👉 func  onDrag <=')
+    let getStyle = window.getComputedStyle(wrapper);
+    let leftVal = parseInt(getStyle.left);
+    let topVal = parseInt(getStyle.top);
+    wrapper.style.left = `${leftVal + movementX}px`;
+    wrapper.style.top = `${topVal + movementY}px`;
 }
-const dragAndDrop = new DragAndDrop("div.selcImg", "body > div.selcImg > div.header");
-dragAndDrop.enable();
+header.addEventListener("mousedown", () => {
+    header.classList.add("activeMove");
+    header.addEventListener("mousemove", onDrag);
+})
+document.addEventListener("mouseup", () => {
+    header.classList.remove("activeMove");
+    header.removeEventListener("mousemove", onDrag);
+})
+wrapper.addEventListener("dblclick", () => {
+    wrapper.style.left = "0px";
+    wrapper.style.top = "0px";
+})
