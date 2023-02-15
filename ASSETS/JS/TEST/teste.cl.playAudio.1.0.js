787f2bb309984f54daf8081bfe65aee5d0f01498
Herbert <herbert.dev.alfa@gmail.com>
2023-02-15 10:53:10 -0300

    🏳️ 01.43 [-g , save]  restart-button

--- ASSETS/JS/TEST/teste.cl.playAudio.1.0.js
+++ ASSETS/JS/TEST/teste.cl.playAudio.1.0.js
@@ -1,5 +1,6 @@
 const audio = document.getElementById('audio');
 const playPauseButton = document.getElementById('play-pause-button');
+const restartButton = document.getElementById('restart-button');
 
 playPauseButton.addEventListener('click', function() {
   if (audio.paused) {
@@ -9,4 +10,10 @@ playPauseButton.addEventListener('click', function() {
     audio.pause();
     playPauseButton.innerHTML = "Play";
   }
+});
+
+restartButton.addEventListener('click', function() {
+  audio.currentTime = 0;
+  audio.pause();
+  playPauseButton.innerHTML = "Play";
 });
