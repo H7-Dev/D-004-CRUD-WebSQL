3aba67299bac903fbbe936d44bfd2a0df425296d
Herbert <herbert.dev.alfa@gmail.com>
2023-02-15 11:40:11 -0300

    🏳️ 01.44 [-g , leaiute]

--- ASSETS/JS/TEST/teste.cl.playAudio.1.0.js
+++ ASSETS/JS/TEST/teste.cl.playAudio.1.0.js
@@ -1,19 +1,36 @@
 const audio = document.getElementById('audio');
 const playPauseButton = document.getElementById('play-pause-button');
 const restartButton = document.getElementById('restart-button');
+const timeDecorrido = document.getElementById('timeDecorrido');
+const timeTotal = document.getElementById('timeTotal');
 
 playPauseButton.addEventListener('click', function() {
   if (audio.paused) {
     audio.play();
-    playPauseButton.innerHTML = "Pause";
+    // playPauseButton.innerHTML = "Pause";
   } else {
     audio.pause();
-    playPauseButton.innerHTML = "Play";
+    // playPauseButton.innerHTML = "Play";
   }
 });
 
 restartButton.addEventListener('click', function() {
-  audio.currentTime = 0;
-  audio.pause();
-  playPauseButton.innerHTML = "Play";
+  if (!audio.paused) {
+    // Se o áudio estiver tocando, apenas reiniciamos a reprodução a partir do início
+    audio.currentTime = 0;
+  } else {
+    // Se o áudio estiver pausado, reiniciamos a reprodução a partir do início e pausamos novamente
+    audio.currentTime = 0;
+    audio.pause();
+    // playPauseButton.innerHTML = "Play";
+  }
+});
+
+audio.addEventListener('timeupdate', function() {
+  const minutes = Math.floor(audio.currentTime / 60);
+  const seconds = Math.floor(audio.currentTime % 60);
+  timeDecorrido.innerHTML = `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`;
+  const durMinutes = Math.floor(audio.duration / 60);
+  const durSeconds = Math.floor(audio.duration % 60);
+  timeTotal.innerHTML = `${durMinutes}:${(durSeconds < 10 ? '0' : '')}${durSeconds}`;
 });
