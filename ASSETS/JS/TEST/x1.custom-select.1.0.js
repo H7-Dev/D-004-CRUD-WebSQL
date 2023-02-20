8ab1a3fb2d2dc9112eea398bc9513170fe0a8dfb
Herbert <herbert.dev.alfa@gmail.com>
2023-02-20 19:34:01 -0300

    01.62 [-g ,save]  custom-select

--- ASSETS/JS/TEST/x1.custom-select.1.0.js
+++ ASSETS/JS/TEST/x1.custom-select.1.0.js
@@ -1,16 +1,47 @@
-const customSelect = document.querySelector('.custom-select');
-const button = customSelect.querySelector('button');
-const optionsList = customSelect.querySelector('ul');
+const selectButton = document.getElementById('select-button');
+const selectOptions = document.getElementById('select-options');
 
-button.addEventListener('click', function() {
-  optionsList.classList.toggle('show');
-  button.classList.toggle('open');
+selectButton.addEventListener('click', () => {
+  selectOptions.classList.toggle('show');
 });
 
-optionsList.querySelectorAll('li').forEach(function(option) {
-  option.addEventListener('click', function() {
-    button.textContent = option.textContent;
-    optionsList.classList.remove('show');
-    button.classList.remove('open');
-  });
+selectOptions.addEventListener('click', (event) => {
+  const selectedOption = event.target;
+  selectButton.textContent = selectedOption.textContent;
+  selectOptions.classList.remove('show');
+});
+
+
+const customSelect = document.querySelector('.custom-select');
+const optionsList = customSelect.querySelector('ul');
+
+customSelect.addEventListener('click', function () {
+  customSelect.classList.toggle('show');
+  if (customSelect.classList.contains('show')) {
+    showOptionsList();
+  } else {
+    hideOptionsList();
+  }
+});
+
+function hideOptionsList() {
+  optionsList.style.opacity = '0';
+  optionsList.style.transition = 'opacity 0.2s ease-in-out';
+  setTimeout(function() {
+    optionsList.style.maxHeight = '0';
+    optionsList.style.visibility = 'hidden';
+  }, 200);
+}
+
+function showOptionsList() {
+  optionsList.style.maxHeight = '200px';
+  optionsList.style.opacity = '1';
+  optionsList.style.visibility = 'visible';
+  optionsList.style.transition = 'opacity 0.2s ease-in-out 0.2s';
+}
+
+customSelect.addEventListener('transitionend', function (event) {
+  if (event.propertyName === 'max-height' && !customSelect.classList.contains('show')) {
+    hideOptionsList();
+  }
 });
