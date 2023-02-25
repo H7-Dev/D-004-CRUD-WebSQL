d69b339d7e9f2aafc82dcadcd9463088383ab27a
Herbert <herbert.dev.alfa@gmail.com>
2023-02-25 17:55:40 -0300

    ❗01.64 [-g ,save]  custom-select
    getBoundingClientRect

--- ASSETS/JS/TEST/x1.custom-select.1.0.js
+++ ASSETS/JS/TEST/x1.custom-select.1.0.js
@@ -2,16 +2,14 @@ const btnOption = document.querySelector("button.btnOption");
 const selcOptions = document.querySelector("ul.selcOptions");
 
 btnOption.addEventListener("click", () => {
-  if (selcOptions.style.display === "block") {
-    selcOptions.style.display = "none";
+  if (selcOptions.classList.contains("show")) {
+    selcOptions.classList.remove("show");
   } else {
-    // Obtém a posição do botão em relação à janela de visualização
     const btnRect = btnOption.getBoundingClientRect();
 
-    // Define as posições da lista em relação ao botão
     selcOptions.style.top = `${btnRect.bottom + 5}px`;
     selcOptions.style.left = `${btnRect.left}px`;
 
-    selcOptions.style.display = "block";
+    selcOptions.classList.add("show"); // Adiciona a classe "show" para exibir a lista
   }
 });
