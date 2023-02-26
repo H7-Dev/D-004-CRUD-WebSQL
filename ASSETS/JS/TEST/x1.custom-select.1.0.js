30cf4b7c4455b2ee6a7c5ab26aa71c7e75b089ed
Herbert <herbert.dev.alfa@gmail.com>
2023-02-26 09:10:52 -0300

    ❗01.67 [-g ,save]  custom-select

--- ASSETS/JS/TEST/x1.custom-select.1.0.js
+++ ASSETS/JS/TEST/x1.custom-select.1.0.js
@@ -11,11 +11,26 @@ btnOption.addEventListener("click", () => {
         const btnRect = btnOption.getBoundingClientRect();
 
         selcOptions.style.top = `${btnRect.bottom + 5}px`;
-        selcOptions.style.left = `${btnRect.left}px`;
+        selcOptions.style.left = `${btnRect.left -5}px`;
 
         // define a largura da lista de seleção igual à largura do botão
         selcOptions.style.width = `${btnWidth}px`;
 
         selcOptions.classList.add("show");
+    }
+});
+
+// adiciona ouvinte de eventos para detectar o clique em um elemento "li"
+selcOptions.addEventListener("click", (event) => {
+    // verifica se o elemento clicado é um "li"
+    if (event.target.tagName === "LI") {
+        // obtém o valor da opção selecionada
+        const selectedValue = event.target.getAttribute("value");
+
+        // atualiza o texto do botão com o valor selecionado
+        btnOption.textContent = selectedValue;
+
+        // oculta a lista de opções
+        selcOptions.classList.remove("show");
     }
 });
