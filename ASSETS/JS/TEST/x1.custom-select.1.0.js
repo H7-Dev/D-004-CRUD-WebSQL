15e84697e28b98a00c7d4b6e27ba657d8a9fbf9d
Herbert <herbert.dev.alfa@gmail.com>
2023-02-26 11:23:23 -0300

    01.70 [-g ,save]   custom-select

--- ASSETS/JS/TEST/x1.custom-select.1.0.js
+++ ASSETS/JS/TEST/x1.custom-select.1.0.js
@@ -1,32 +1,44 @@
 const btnOpts = document.querySelector("button.btnOpts");
 const ulOptions = document.querySelector("ul.ulOptions");
+const inOpts = document.querySelector("input.in_opts");
 
 btnOpts.addEventListener("click", () => {
-    // obtém a largura do botão
-    const btnWidth = btnOpts.offsetWidth;
+  // obtém a largura do botão
+  const btnWidth = btnOpts.offsetWidth;
 
-    if (ulOptions.classList.contains("show")) {
-        ulOptions.classList.remove("show");
-        btnOpts.classList.remove("active");
-    } else {
-        const btnRect = btnOpts.getBoundingClientRect();
+  if (ulOptions.classList.contains("show")) {
+    ulOptions.classList.remove("show");
+    btnOpts.classList.remove("active");
+  } else {
+    const btnRect = btnOpts.getBoundingClientRect();
 
-        ulOptions.style.top = `${btnRect.bottom + 5}px`;
-        ulOptions.style.left = `${btnRect.left}px`;
+    ulOptions.style.top = `${btnRect.bottom + 5}px`;
+    ulOptions.style.left = `${btnRect.left}px`;
 
-        // define a largura da lista de seleção igual à largura do botão
-        ulOptions.style.width = `${btnWidth}px`;
+    // define a largura da lista de seleção igual à largura do botão
+    ulOptions.style.width = `${btnWidth}px`;
 
-        ulOptions.classList.add("show");
-        btnOpts.classList.add("active");
-    }
+    ulOptions.classList.add("show");
+    btnOpts.classList.add("active");
+  }
 });
 
 document.addEventListener("click", (event) => {
-    const isClickInside = btnOpts.contains(event.target) || ulOptions.contains(event.target);
+  const isClickInside = btnOpts.contains(event.target) || ulOptions.contains(event.target);
 
-    if (!isClickInside) {
-        ulOptions.classList.remove("show");
-        btnOpts.classList.remove("active");
-    }
+  if (!isClickInside) {
+    ulOptions.classList.remove("show");
+    btnOpts.classList.remove("active");
+  }
+});
+
+const liOptions = document.querySelectorAll("ul.ulOptions li");
+
+liOptions.forEach(li => {
+  li.addEventListener("click", () => {
+    btnOpts.textContent = li.textContent;
+    inOpts.value = li.getAttribute("value");
+    ulOptions.classList.remove("show");
+    btnOpts.classList.remove("active");
+  });
 });
