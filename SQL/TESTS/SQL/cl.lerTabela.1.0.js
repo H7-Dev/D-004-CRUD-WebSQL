70b76043ad88c659801b51d795074b03be31119a
Herbert <herbert.dev.alfa@gmail.com>
2023-03-06 16:52:10 -0300

    1.86 [-g-s] cl.lerTabela.1.0

--- SQL/TESTS/SQL/cl.lerTabela.1.0.js
+++ SQL/TESTS/SQL/cl.lerTabela.1.0.js
@@ -1,22 +1,30 @@
-function obterDados() {
-    db.transaction(function (tx) {
-        tx.executeSql('SELECT rowid, * FROM tbPessoas', [], function (tx, resultado) {
-            const tbody = document.querySelector('.tbody');
-            let html = '';
-            for (let i = 0; i < resultado.rows.length; i++) {
-                const row = resultado.rows.item(i);
-                html += `<tr>
-                    <td>${row.rowid}</td>
-                    <td>${row.idPessoas}</td>
-                    <td>${row.c_nome}</td>
-                    <td>${row.c_email}</td>
-                    <td>${row.c_date}</td>
-                    <td>${row.c_pais}</td>
-                    <td>${row.c_sexo}</td>
-                </tr>`;
-            }
-            tbody.innerHTML = html;
+class TabelaPessoas {
+    constructor(db) {
+      this.db = db;
+      this.tbody = document.querySelector('.tbody');
+    }
+
+    exibirDados() {
+        console.log('TabelaPessoas')
+      this.db.transaction((tx) => {
+        tx.executeSql('SELECT rowid, * FROM tbPessoas', [], (tx, resultado) => {
+          let html = '';
+          for (let i = 0; i < resultado.rows.length; i++) {
+            const row = resultado.rows.item(i);
+            html += `<tr>
+              <td>${row.rowid}</td>
+              <td>${row.idPessoas}</td>
+              <td>${row.c_nome}</td>
+              <td>${row.c_email}</td>
+              <td>${row.c_date}</td>
+              <td>${row.c_pais}</td>
+              <td>${row.c_sexo}</td>
+            </tr>`;
+          }
+          this.tbody.innerHTML = html;
         });
-    });
-}
-obterDados();
+      });
+    }
+  }
+  const tabela = new TabelaPessoas(db);
+  tabela.exibirDados();
